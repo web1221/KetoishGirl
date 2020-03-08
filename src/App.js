ec5b293020aa1b706211e720dd92225e23700316
Lela Smith <smith.lelaa@gmail.com>
2020-03-07 19:51:31 -0800

    add routes for all files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
+import KetoBasics from './components/KetoBasics';
+import MyStory from './components/MyStory';
+import Resources from './components/Resources';
+import ScheduleAppointment from './components/ScheduleAppointment';
 import RaceList from './components/RaceList';
 import Footer from './components/Footer';
 import EditRace from './components/EditRace';
@@ -19,7 +23,10 @@ function App(){
       <Route path='/editrace/:id' component={EditRace} />
       <Route path='/createrace' component={CreateRace} />
       <Route path='/showrace/:id' component={ShowRace} />
-      <Route exact path='/racelist' component={RaceList} />
+      <Route exact path='/ketobasics' component={KetoBasics} />
+      <Route exact path='/mystory' component={MyStory} />
+      <Route exact path='/resources' component={Resources} />
+      <Route exact path='/scheduleappointment' component={ScheduleAppointment} />
       </Switch>
       <Footer />
       </div>
---
+++ src/components/KetoBasics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function KetoBasics(){
+
+  return (
+    <div>
+    <h1>KetoBasics Page</h1>
+    </div>
+  );
+}
+
+export default KetoBasics;
